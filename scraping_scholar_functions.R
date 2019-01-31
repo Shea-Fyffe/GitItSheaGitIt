5a954992f69e3d434884b7b439f9de197f3b6df2
Shea Fyffe <34585011+Shea-Fyffe@users.noreply.github.com>
2019-01-31 12:48:24 -0500

    Added roxygen code
    Shea Fyffe: removed sys.sleep
    Shea Fyffe: still has issues pulling more than 1000 articles

--- scraping_scholar_functions.R
+++ scraping_scholar_functions.R
@@ -19,19 +19,20 @@ scrape_scholar <- function(...) {
   .out <- do.call("rbind", .out)
   return(.out)
 }
+
+
 #' @Title Build Search
 #' @author Shea Fyffe sfyffe@@gmail.com
 #' @description Helper function for scraping Google Scholar
-#' @param ... String to be passed to Google Scholar search. Use \code{max = } and \code{year = } to restirct number of articles returned as well as year.
+#' @param ... String to be passed to Google Scholar search. Use \code{min =}, \code{max = } and \code{year = } to restirict number of articles returned as well as year.
 #' @examples \dontrun{
 #'                    ## Searches for 'Engagement' AND 'organizational behavior' and limits return to 250 
 #'                    res <- build_search("Engagement", "organizational behavior", max = 250)}
 #'           \dontrun{
-#'                    ## Searches for articles newer that 2010 with the words 'Job satistfaction' and limits return to 200 
-#'                    res <- build_search("Job satistfaction", year = 2010, max = 200)}
+#'                    ## Searches for articles newer that 2010 with the words 'Job satistfaction' and limits return to 200 starts at 100
+#'                    res <- build_search("Job satistfaction", year = 2010, min = 100, max = 200)}
 #'@seealso \code{scrape_scholar}
 #'@export
-
 build_search <- function(...) {
   .search <- list(...)
   if("year" %in% names(.search)){
@@ -40,37 +41,56 @@ build_search <- function(...) {
   }
   if("max" %in% names(.search)){
     .max <- .search[["max"]]
+    if(as.numeric(.max) %% 10 != 0) {
+      stop("Please verify max is divisible by 10")
+    }
     .search[["max"]] <- NULL
   }
+  if("min" %in% names(.search)){
+    .min <- .search[["min"]]
+    if(as.numeric(.min) %% 10 != 0) {
+      stop("Please verify min is divisible by 10")
+    }
+    .search[["min"]] <- NULL
+  }
+  .args <- c(exists(".year"),exists(".max"),exists(".min"))
   .search <- sapply(.search, utils::URLencode)
   .search <- paste(.search, collapse = "+")
-  if(exists(".year") & exists(".max")) {
-    .max <- seq(0, as.numeric(.max), by = 10)
+  if(all(.args)) {
+    .max <- seq(as.numeric(.min), as.numeric(.max), by = 10)
+    .base <- sprintf("https://scholar.google.com/scholar?start=%dhl=en&as_vis=1&as_sdt=%%2C47&as_ylo=%s&q=%s&btnG=", .max, .year, .search)
+  } else if (.args[1] & !.args[2] & !.args[3]) {
+    .max <- seq(0, 100, by = 10)
     .base <- sprintf("https://scholar.google.com/scholar?start=%dhl=en&as_vis=1&as_sdt=%%2C47&as_ylo=%s&q=%s&btnG=", .max, .year,.search)
-  } else if (exists(".year") & !exists(".max")) {
-    .max <- seq(0, 200, by = 10)
-    .base <- sprintf("https://scholar.google.com/scholar?start=%dhl=en&as_vis=1&as_sdt=%%2C47&as_ylo=%s&q=%s&btnG=", .max, .year,.search)
-  } else if (!exists(".year") & exists(".max")) {
+  } else if (!.args[1] & (.args[2] & .args[3])) {
+    .max <- seq(as.numeric(.min), as.numeric(.max), by = 10)
+    .base <- sprintf("https://scholar.google.com/scholar?start=%d&hl=en&as_vis=1&as_sdt=0%%2C47&q=%s&btnG=", .max, .search)
+  } else if (!.args[1] & .args[2] & !.args[3]) {
     .max <- seq(0, as.numeric(.max), by = 10)
     .base <- sprintf("https://scholar.google.com/scholar?start=%d&hl=en&as_vis=1&as_sdt=0%%2C47&q=%s&btnG=", .max, .search)
+  } else if (!.args[1] & !.args[2] & .args[3]) {
+    .max <- seq(as.numeric(.min), as.numeric(.min) + 100, by = 10)
+    .base <- sprintf("https://scholar.google.com/scholar?start=%d&hl=en&as_vis=1&as_sdt=0%%2C47&q=%s&btnG=", .max, .search)
+  } else if (.args[1] & .args[2] & !.args[3]) {
+    .max <- seq(0, as.numeric(.max), by = 10)
+    .base <- sprintf("https://scholar.google.com/scholar?start=%dhl=en&as_vis=1&as_sdt=%%2C47&as_ylo=%s&q=%s&btnG=", .max, .year,.search)
+  } else if (.args[1] & !.args[2] & .args[3]) {
+    .max <- seq(as.numeric(.min), as.numeric(.min) + 100, by = 10)
+    .base <- sprintf("https://scholar.google.com/scholar?start=%dhl=en&as_vis=1&as_sdt=%%2C47&as_ylo=%s&q=%s&btnG=", .max, .year,.search)
   } else {
-    .max <- seq(0, 200, by = 10)
+    .max <- seq(0, 100, by = 10)
     .base <- sprintf("https://scholar.google.com/scholar?start=%d&hl=en&as_vis=1&as_sdt=0%%2C47&q=%s&btnG=", .max, .search)
   }
   return(.base)
 }
 
-n_pages <- function(html_page) { 
-  .pages <- rvest::html_nodes(html_page, '.gs_ab_mdw')
-  .pages <- rvest::html_text(.pages)[2]
-  .pages <-  gsub(",", "", sub("About (.*?) results.*","\\1", .pages))
-  .pages <- as.numeric(.pages) / 10
-  return(.pages)
-  }
 
+#'@Title Scrape Child Function
+#'@Description This function actually does the scraping of Google Scholar
+#'@export
+#'@import curl rvest xml2
+NULL
 scrape <- function(url, verbose = FALSE){
-  #download.file(url, destfile = "temp.html", quiet=TRUE)
-  #my_page <- xml2::read_html("temp.html")
   my_page <- xml2::read_html(curl::curl(url, handle = curl::new_handle("useragent" = "Chrome/60.0.3112.113")))
     if(verbose){
       return(xml2::html_structure(my_page))
@@ -98,9 +118,16 @@ scrape <- function(url, verbose = FALSE){
         if(all(sapply(.out, length)==length(.out[[1]]))){
           .out <- as.data.frame(.out, stringsAsFactors = FALSE)
         }
-        on.exit(file.remove("temp.html"))
         return(.out)
     }
 }
 
 
+#'@Title just a helper to get the number of pages from a search
+n_pages <- function(html_page) { 
+  .pages <- rvest::html_nodes(html_page, '.gs_ab_mdw')
+  .pages <- rvest::html_text(.pages)[2]
+  .pages <-  gsub(",", "", sub("About (.*?) results.*","\\1", .pages))
+  .pages <- as.numeric(.pages) / 10
+  return(.pages)
+  }
