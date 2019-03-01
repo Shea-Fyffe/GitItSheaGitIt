4f9e6eab1f8095a8f63368539827c3ca1280c5ad
Shea-Fyffe <shea.fyffe@gmail.com>
2019-03-01 16:32:56 -0500

    Shea-Fyffe: Added between year functionality. Upped default number of articles pulled to 250. Added helping functions to clean strings.

--- scraping_scholar_functions.R
+++ scraping_scholar_functions.R
@@ -15,18 +15,19 @@ scrape_scholar <- function(...) {
   .out <- list()
   for(i in seq(length(.url))) {
     .out[[i]] <- try(scrape(.url[i]))
-    if(inherits(.out[[i]], "try-error") & i > 1) {
+    if((inherits(.out[[i]], "try-error") || is.null(.out[[i]])) & i > 1) {
       return(.out[[-i]])
     } else if (inherits(.out[[i]], "try-error") & i == 1){
       stop(sprintf("open:%s and do the CAPTCHA", .url))
+    } else {
+      .n <- as.numeric(sub('.*?start=(\\d+)hl.*', '\\1', .url[i]))
+      .n <- seq(.n + 1L, .n + 10L)
+      .out[[i]]$Article_Number <- .n
+      Sys.sleep(sample(5, 1))
     }
-    #TODO(FIX THIS)
-    #sub('.*?start=(\\d+)&.*', '\\1', .url)
-    Sys.sleep(sample(5, 1))
   }
   return(.out)
 }
-
 #' @Title Build Search
 #' @author Shea Fyffe sfyffe@@gmail.com
 #' @description Helper function for scraping Google Scholar
@@ -38,6 +39,9 @@ scrape_scholar <- function(...) {
 #'           \dontrun{
 #'                    ## Searches for articles newer that 2010 with the words 'Job satistfaction' and limits return to 200 starts at 100
 #'                    res <- build_search("Job satistfaction", year = 2010, min = 100, max = 200)}
+#'           \dontrun{
+#'                    ## Searches for articles newer that 2010 but earlier than 2015 with the words 'Job satistfaction' and limits return to 200 starts at 100
+#'                    res <- build_search("Job satistfaction", year = c(2010,2015), min = 100, max = 200)}                    
 #'@seealso \code{scrape_scholar}
 #'@export
 build_search <- function(...) {
@@ -65,10 +69,18 @@ build_search <- function(...) {
   .search <- paste(.search, collapse = "+")
   if(all(.args)) {
     .max <- seq(as.numeric(.min), as.numeric(.max), by = 10)
-    .base <- sprintf("https://scholar.google.com/scholar?start=%dhl=en&as_vis=1?&as_sdt=%%2C47&as_ylo=%s&q=%s&btnG=", .max, .year, .search)
+    if(length(.year)==2L) {
+      .base <- sprintf("https://scholar.google.com/scholar?start=%dhl=en&as_vis=1?&as_sdt=%%2C47&as_ylo=%s&as_yhi=%s&q=%s&btnG=", .max, .year[1], .year[2], .search)
+    } else {
+      .base <- sprintf("https://scholar.google.com/scholar?start=%dhl=en&as_vis=1?&as_sdt=%%2C47&as_ylo=%s&q=%s&btnG=", .max, .year, .search) 
+    }
   } else if (.args[1] & !.args[2] & !.args[3]) {
-    .max <- seq(0, 100, by = 10)
-    .base <- sprintf("https://scholar.google.com/scholar?start=%dhl=en&as_vis=1&as_sdt=%%2C47&as_ylo=%s&q=%s&btnG=", .max, .year,.search)
+    .max <- seq(0, 250, by = 10)
+    if(length(.year)==2L) {
+      .base <- sprintf("https://scholar.google.com/scholar?start=%dhl=en&as_vis=1?&as_sdt=%%2C47&as_ylo=%s&as_yhi=%s&q=%s&btnG=", .max, .year[1], .year[2], .search)
+    } else {
+      .base <- sprintf("https://scholar.google.com/scholar?start=%dhl=en&as_vis=1?&as_sdt=%%2C47&as_ylo=%s&q=%s&btnG=", .max, .year, .search) 
+    }
   } else if (!.args[1] & (.args[2] & .args[3])) {
     .max <- seq(as.numeric(.min), as.numeric(.max), by = 10)
     .base <- sprintf("https://scholar.google.com/scholar?start=%d&hl=en&as_vis=1&as_sdt=0%%2C47&q=%s&btnG=", .max, .search)
@@ -76,16 +88,24 @@ build_search <- function(...) {
     .max <- seq(0, as.numeric(.max), by = 10)
     .base <- sprintf("https://scholar.google.com/scholar?start=%d&hl=en&as_vis=1&as_sdt=0%%2C47&q=%s&btnG=", .max, .search)
   } else if (!.args[1] & !.args[2] & .args[3]) {
-    .max <- seq(as.numeric(.min), as.numeric(.min) + 100, by = 10)
+    .max <- seq(as.numeric(.min), as.numeric(.min) + 250, by = 10)
     .base <- sprintf("https://scholar.google.com/scholar?start=%d&hl=en&as_vis=1&as_sdt=0%%2C47&q=%s&btnG=", .max, .search)
   } else if (.args[1] & .args[2] & !.args[3]) {
     .max <- seq(0, as.numeric(.max), by = 10)
-    .base <- sprintf("https://scholar.google.com/scholar?start=%dhl=en&as_vis=1&as_sdt=%%2C47&as_ylo=%s&q=%s&btnG=", .max, .year,.search)
+    if(length(.year)==2L) {
+      .base <- sprintf("https://scholar.google.com/scholar?start=%dhl=en&as_vis=1?&as_sdt=%%2C47&as_ylo=%s&as_yhi=%s&q=%s&btnG=", .max, .year[1], .year[2], .search)
+    } else {
+      .base <- sprintf("https://scholar.google.com/scholar?start=%dhl=en&as_vis=1?&as_sdt=%%2C47&as_ylo=%s&q=%s&btnG=", .max, .year, .search) 
+    }
   } else if (.args[1] & !.args[2] & .args[3]) {
-    .max <- seq(as.numeric(.min), as.numeric(.min) + 100, by = 10)
-    .base <- sprintf("https://scholar.google.com/scholar?start=%dhl=en&as_vis=1&as_sdt=%%2C47&as_ylo=%s&q=%s&btnG=", .max, .year,.search)
+    .max <- seq(as.numeric(.min), as.numeric(.min) + 250, by = 10)
+    if(length(.year)==2L) {
+      .base <- sprintf("https://scholar.google.com/scholar?start=%dhl=en&as_vis=1?&as_sdt=%%2C47&as_ylo=%s&as_yhi=%s&q=%s&btnG=", .max, .year[1], .year[2], .search)
+    } else {
+      .base <- sprintf("https://scholar.google.com/scholar?start=%dhl=en&as_vis=1?&as_sdt=%%2C47&as_ylo=%s&q=%s&btnG=", .max, .year, .search) 
+    }
   } else {
-    .max <- seq(0, 100, by = 10)
+    .max <- seq(0, 250, by = 10)
     .base <- sprintf("https://scholar.google.com/scholar?start=%d&hl=en&as_vis=1&as_sdt=0%%2C47&q=%s&btnG=", .max, .search)
   }
   return(.base)
@@ -110,8 +130,6 @@ scrape <- function(url, user_agent = NULL, verbose = FALSE){
         on.exit(closeAllConnections())
         return(.out)
 }
-
-
 #'@Title just a helper to get the number of pages from a search
 n_pages <- function(html_page) { 
   .pages <- rvest::html_nodes(html_page, '.gs_ab_mdw')
@@ -120,7 +138,6 @@ n_pages <- function(html_page) {
   .pages <- as.numeric(.pages) / 10
   return(.pages)
 }
-
 #'@Title just a helper that finds if articles are missing nodes (i.e., Abstracts, links, etc.)
 #'@export
 #'@import rvest xml2
@@ -139,63 +156,59 @@ parse_page <- function(.html) {
   .html <- .html[c("Title", "Author", "Journal", "Year", "Link", "Abstract")]
   
   return(.html)
-  
-  
-  #children
-  parsing_helper <- function(.vec) {
-    .vec <- gsub("(?<=[\\s])\\s*|^\\s+|\\s+$", "", .vec, perl=TRUE)
-    return(.vec)
-  }
-  .parsing_helper <- function(.df) {
-    .meta <- strsplit(.df$Meta, "-")
-    .meta <- lapply(.meta, .f)
-    .meta <- do.call("rbind", .meta)
-    .df$Title <- gsub("\\[[^\\]]*\\]", "", .df$Title, perl=TRUE)
-    .df$Abstract <- gsub("[^[:alnum:][:blank:]+?&/\\-]", " ", .df$Abstract, perl=TRUE)
-    .df$Abstract <- gsub("^Page\\s\\d+.", "", .df$Abstract)
-    .df$Meta <- NULL
-    .df <- cbind(.df, .meta)
-    return(.df)
-  }
-  .f <- function(.vec) {
-    if(length(.vec == 3)||length(.vec == 2)) {
-      Author <- gsub("\\s-.*", "", .vec[1])
-      if(grepl(",", .vec[2])) {
-        Date <- gsub(".*(\\d{4})", "\\1", .vec[2])
-        Journal <- gsub("^\\s(.+),.*", "\\1", .vec[2])
-      } else if (grepl("^\\d{4}$|^\\s\\d{4}$", .vec[2])) {
-        Date <- gsub(".*(\\d{4})", "\\1", .vec[2])
-        Journal <- NA
-      } else if (!grepl("^\\d{4}$|^\\s\\d{4}$", .vec[2])) {
-        Date <- NA
-        Journal <- .vec[2]
-      } else {
-        Date <- NA
-        Journal <- NA
-      }
-    } else if(length(.vec == 1)) {
-      if(grepl("([A-Z]+ )", .vec[1])) {
-        Author <- .vec[1]
-        Date <- NA
-        Journal <- NA
-      } else if(grepl("^\\d{4}$|^\\s\\d{4}$", .vec[1])) {
-        Author <- NA
-        Date <- gsub(".*(\\d{4})", "\\1", .vec[1])
-        Journal <- NA
-      } else if(!grepl("\\.", .vec[1])) {
-        Author <- NA
-        Date <- NA
-        Journal <- .vec[1]
-      } else {
-        Author <- NA
-        Date <- NA
-        Journal <- NA
-      }
-    }
-    .res <- data.frame(Author = Author, Journal = Journal, Year = Date, stringsAsFactors = FALSE)
-    return(.res)
-  }
-  
-  
 }
-
+####children
+parsing_helper <- function(.vec) {
+  .vec <- gsub("(?<=[\\s])\\s*|^\\s+|\\s+$", "", .vec, perl=TRUE)
+  return(.vec)
+}
+.parsing_helper <- function(.df) {
+  .meta <- strsplit(.df$Meta, "\\s-\\s")
+  .meta <- lapply(.meta, .f)
+  .meta <- do.call("rbind", .meta)
+  .df$Title <- gsub("\\[[^\\]]*\\]", "", .df$Title, perl=TRUE)
+  .df$Abstract <- gsub("[^[:alnum:][:blank:]+?&/\\-]", " ", .df$Abstract, perl=TRUE)
+  .df$Abstract <- gsub("^Page\\s\\d+.", "", .df$Abstract)
+  .df$Meta <- NULL
+  .df <- cbind(.df, .meta)
+  return(.df)
+}
+#' @details helper that extracts Author, Journal, and Year
+.f <- function(.vec) {
+  if(length(.vec) == 3||length(.vec) == 2) {
+    Author <- gsub("\\s-.*", "", .vec[1])
+    if(grepl(",", .vec[2])) {
+      Date <- gsub(".*(\\d{4})", "\\1", .vec[2])
+      Journal <- gsub("(.+?)(\\,.*)", "\\1", .vec[2])
+    } else if (grepl("^\\d{4}$|^\\s\\d{4}$", .vec[2])) {
+      Date <- gsub(".*(\\d{4})", "\\1", .vec[2])
+      Journal <- NA
+    } else if (!grepl("^\\d{4}$|^\\s\\d{4}$", .vec[2])) {
+      Date <- NA
+      Journal <- .vec[2]
+    } else {
+      Date <- NA
+      Journal <- NA
+    }
+  } else if(length(.vec == 1)) {
+    if(grepl("([A-Z]+ )", .vec[1])) {
+      Author <- .vec[1]
+      Date <- NA
+      Journal <- NA
+    } else if(grepl("^\\d{4}$|^\\s\\d{4}$", .vec[1])) {
+      Author <- NA
+      Date <- gsub(".*(\\d{4})", "\\1", .vec[1])
+      Journal <- NA
+    } else if(!grepl("\\.", .vec[1])) {
+      Author <- NA
+      Date <- NA
+      Journal <- .vec[1]
+    } else {
+      Author <- NA
+      Date <- NA
+      Journal <- NA
+    }
+  }
+  .res <- data.frame(Author = Author, Journal = Journal, Year = Date, stringsAsFactors = FALSE)
+  return(.res)
+}
