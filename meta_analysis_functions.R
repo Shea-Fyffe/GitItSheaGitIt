12cd7d6664168d56ab80eace72fb308d18eed5ea
Shea-Fyffe <shea.fyffe@gmail.com>
2019-04-20 20:41:21 -0400

    Shea Fyffe: added synonym identifier

--- meta_analysis_functions.R
+++ meta_analysis_functions.R
@@ -44,3 +44,28 @@ get_pdf_text<- function(.dir = getwd(), ...){
   .res <- lapply(.res, Trim)
   return(.res)
 }
+
+#' @title Identify synonyms using wordnet
+#' @author Shea Fyffe, \email{shea.fyffe@@gmail.com}
+#' @param x character vector of situational adejectives
+#' @param POS part-of-speech to be identified
+#' @param dictionary a file path wordnet dictionary
+#' @param drop if false, will return all synonyms identified
+#' @seealso [wordnet::setDict()]
+#' @import reshape2 wordnet
+#' @export
+synonym_match <- function(x, POS = "ADJECTIVE", dictionary = "C:\\Program Files (x86)\\WordNet\\2.1\\dict", drop = TRUE) {
+  .home <- gsub("*\\\\dict", "", dictionary)
+  Sys.setenv(WNHOME = .home)
+  wordnet::setDict(dictionary)
+  .syn <- sapply(x, function(x) wordnet::synonyms(word = x, pos = POS))
+  .syn <- reshape2::melt(.syn, factorsAsStrings = FALSE)
+  names(.syn) <- c("Synonym", "Word")
+  .syn[,1] <- gsub("*\\([^\\)]+\\)$","",as.character(.syn[,1]))
+  .syn[,"match"] <- ifelse(.syn[,1]!=.syn[,2], T,F)
+  .syn <- .syn[.syn[,"match"],]
+  if(drop) {
+    .syn <- .syn[vec_grep(.syn[,2],.syn[,1], FALSE),]
+  }
+  .syn
+}
