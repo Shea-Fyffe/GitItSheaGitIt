37039f7dd24ffad75f0b100c7ccf9db192f5f054
Shea-Fyffe <shea.fyffe@gmail.com>
2019-09-11 15:52:34 -0400

    updates

---
+++ text_functions.R
@@ -0,0 +1,167 @@
+#' @title Convert PDF File to Text
+#'
+#' @author Shea Fyffe, \email{shea.fyffe@@gmail.com}
+#'
+#'
+#' @param .dir file directory of pdf(s)
+#' @param ... a character vector to filter out PDF file names
+#'
+#' @examples \example{
+#' \dontrun{
+#'   \code{pdf_text_files <- get_pdf_text(.dir = "%systemdrive%/Documents and Settings/All Users/Desktop", "words in pdf file name")
+#'         cat(pdf_text_files)}
+#'         }
+#' \dontrun{
+#'   \code{pdf_text_files <- get_pdf_text(.dir = "%systemdrive%/Documents and Settings/All Users/Desktop", c("Employee", "Engagement"))
+#'         cat(pdf_text_files)}
+#'         }
+#'
+#' @import pdftools readr
+#' @export
+get_pdf_text <- function(.dir = getwd(), clean = TRUE, ...) {
+  .paths <- tryCatch(
+    list.files(.dir, pattern = "\\.pdf$"),
+    error = function(err) {
+      NA
+    }
+  )
+  if (!length(.paths) || is.na(.paths)) {
+    stop(sprintf("No valid PDF files found in %s", dir))
+  }
+  .fuzzy <- list(...)
+  if (!!length(.fuzzy)) {
+    .fuzzy <- paste(.fuzzy, collapse = "|")
+    if (any(grepl(.fuzzy, x = .paths, ignore.case = TRUE))) {
+      .paths <- .paths[grepl(.fuzzy, x = .paths, ignore.case = TRUE)]
+    }
+  }
+  .pdfs <- lapply(.paths, pdftools::pdf_text)
+  if (clean) {
+    .pdfs <- lapply(.pdfs, clean_pdf)
+  }
+  return(.pdfs)
+}
+#' @title Identify synonyms using wordnet
+#' @author Shea Fyffe, \email{shea.fyffe@@gmail.com}
+#' @param x character vector of situational adejectives
+#' @param POS part-of-speech to be identified
+#' @param dictionary a file path wordnet dictionary
+#' @param drop if false, will return all synonyms identified
+#' @seealso [wordnet::setDict()]
+#' @import reshape2 wordnet
+#' @export
+synonym_match <-
+  function(x,
+           POS = "ADJECTIVE",
+           dictionary = "C:\\Program Files (x86)\\WordNet\\2.1\\dict",
+           drop = TRUE) {
+    .home <- gsub("*\\\\dict", "", dictionary)
+    Sys.setenv(WNHOME = .home)
+    wordnet::setDict(dictionary)
+    .syn <-
+      sapply(x, function(x)
+        wordnet::synonyms(word = x, pos = POS))
+    .syn <- reshape2::melt(.syn, factorsAsStrings = FALSE)
+    names(.syn) <- c("Synonym", "Word")
+    .syn[, 1] <- gsub("*\\([^\\)]+\\)$", "", as.character(.syn[, 1]))
+    .syn[, "match"] <- ifelse(.syn[, 1] != .syn[, 2], T, F)
+    .syn <- .syn[.syn[, "match"], ]
+    if (drop) {
+      .syn <- .syn[vec_grep(.syn[, 2], .syn[, 1], FALSE), ]
+    }
+    .syn
+  }
+#' @title Count common words between two vectors
+#' @author Shea Fyffe, \email{shea.fyffe@@gmail.com}
+#' @param x character vector of words or sentences.
+#' @param y character vector of words or sentences.
+#' @param stopwords Logical. Remove stop words? Uses [tm::stopwords]
+#' @param stem Logical. Stem words? Uses [textstem::stem_word]
+#' @import tm textstem
+#' @export
+count_words <- function(x, y, stopwords = TRUE, stem = FALSE) {
+  if (typeof(x) != "character" || typeof(y) != "character") {
+    stop("Please define x and y as a character")
+  }
+  if (stopwords) {
+    sw <- paste(tm::stopwords("en"), collapse = "\\b|\\b")
+    sw <- paste0("\\b", sw, "\\b")
+    x <- gsub(sw, " ", x)
+    y <- gsub(sw, " ", y)
+  }
+  if (stem) {
+    x <- textstem::stem_words(x, "en")
+    y <- textstem::stem_words(y, "en")
+  }
+  id_x <- paste("X", seq_along(x), sep = "")
+  id_y <- paste("Y", seq_along(y), sep = "")
+  l <- sapply(list(x, y), clean_text)
+  l <- sapply(l, function(x)
+    strsplit(x, split = " "))
+  ll <- lapply(l[[1]], function(x)
+    sapply(l[[2]], function(y) length(intersect(x, y)), simplify = T)
+  )
+  # res <- as.data.frame(res)
+  # names(res) <- unique(x)
+  # res[, "doc_y"] <- unique(y)
+  # res <- tidyr::gather_(res, "doc_x", "common_word_count",
+  #                       names(res)[names(res) != "doc_y"], na.rm = T)
+  return(ll)
+}
+#' @title Wrap Text Function
+#' @export
+wrap_text <- function(txt, pattern) {
+  if (any(nchar(txt) == 0L)) {
+    txt <- txt[!nchar(txt) == 0L]
+  }
+  lines <- grep(pattern, txt)
+  remove <- setdiff(seq(txt), lines)
+  d <- diff(lines)
+  spread <- unique(d)[order(unique(d), decreasing = TRUE)]
+  for (i in seq(spread)) {
+    wrap <- lines[d == spread[i]]
+    if (spread[i] != 1L && (spread[i] - 1L) %in% spread) {
+      txt[wrap] <- paste(txt[wrap], txt[wrap + 1L], sep = " ")
+    } else if (spread[i] == 1L) {
+      txt[wrap] <- txt[wrap]
+    } else {
+      txt[wrap + 1L] <- NA
+    }
+  }
+  txt <- txt[-remove]
+  return(txt)
+}
+#' @title Clean text from character vector
+#' @author Shea Fyffe, \email{shea.fyffe@@gmail.com}
+#' @param x character vector of words or sentences.
+#' @import qdap
+#' @export
+clean_text <- function(x) {
+  if (typeof(x) != "character") {
+    stop("Please define x as a character")
+  }
+  x <- gsub("[^[:alnum:]\\-\\'\\s\\.]", " ", x)
+  x <- gsub("\\.$", "", x)
+  if (any(grepl("\\.", x))) {
+    message("text contains '.' not at end splitting data")
+    x <- strsplit(x, ".")
+    x <- unlist(x)
+  }
+  x <- gsub("\\s+", " ", x)
+  if (any(grepl("[0-9]", x))) {
+    x <- qdap::replace_number(x)
+    x <- qdap::replace_ordinal(x)
+  }
+  x <- gsub("^\\s+|\\s+$", "", x)
+  x <- tolower(x)
+  return(x)
+}
+#' @title Capture text between two characters
+#' @author Shea Fyffe, \email{shea.fyffe@@gmail.com}
+#' @param x character vector of words or sentences.
+#' @param between character vector of length 2 containing boundary characters 
+extract_text_between <- function(x, between = c(".*", ".*")) {
+  .pattern <- sprintf("%s(.*?)%s", between[1], between[2])
+  .x <- regmatches(x, regexec(.pattern, x))
+  return(.x)
+}
--- utility_functions.R
+++ utility_functions.R
@@ -14,23 +14,26 @@
 #'
 #' @family Utilities
 #' @export
-age.calc <- function(dob, enddate=Sys.Date(), units= "years"){
+age.calc <- function(dob,
+                     enddate = Sys.Date(),
+                     units = "years") {
   if (!inherits(dob, "Date") | !inherits(enddate, "Date"))
     stop("Both dob and enddate must be Date class objects")
   start <- as.POSIXlt(dob)
   end <- as.POSIXlt(enddate)
-
+  
   years <- end$year - start$year
-  if(units=="years"){
+  if (units == "years") {
     result <- ifelse((end$mon < start$mon) |
-                       ((end$mon == start$mon) & (end$mday < start$mday)),
+                       ((end$mon == start$mon) &
+                          (end$mday < start$mday)),
                      years - 1, years)
-  }else if(units=="months"){
-    months <- (years-1) * 12
+  } else if (units == "months") {
+    months <- (years - 1) * 12
     result <- months + start$mon
-  }else if(units=="days"){
-    result <- difftime(end, start, units='days')
-  }else{
+  } else if (units == "days") {
+    result <- difftime(end, start, units = 'days')
+  } else{
     stop("Unrecognized units. Please choose years, months, or days.")
   }
   return(result)
@@ -56,55 +59,99 @@ age.calc <- function(dob, enddate=Sys.Date(), units= "years"){
 #'
 #' @family Utilities
 #' @export
-fuzzy.match <- function(x, y = NULL, threshold = 2.5, ...){
-  if(missing(x)||!is.character(x)) stop("Please define x as a character vector")
-  if(!is.null(y) & !is.character(y)) stop("Please define y as a character vector")
-  if(anyNA(x)){
+fuzzy.match <- function(x,
+                        y = NULL,
+                        threshold = 2.5,
+                        ...) {
+  if (missing(x) ||
+      !is.character(x))
+    stop("Please define x as a character vector")
+  if (!is.null(y) &
+      !is.character(y))
+    stop("Please define y as a character vector")
+  if (anyNA(x)) {
     x <- na.omit(x)
   }
   x <- gsub("[[:punct:]]", "", x)
-  if(object.size(x) > 500 * 1000) stop("x is too large, please subsection the vector")
-  if(is.null(y)) {
+  if (object.size(x) > 500 * 1000)
+    stop("x is too large, please subsection the vector")
+  if (is.null(y)) {
     y <- unique(x)
   } else {
-  y <- gsub("[[:punct:]]", "", y)
-  if(any(duplicated(y))) {
-    warning("y is not unique, converting to unique vector")
-    if(anyNA(y)){
-      y <- na.omit(y)
+    y <- gsub("[[:punct:]]", "", y)
+    if (any(duplicated(y))) {
+      warning("y is not unique, converting to unique vector")
+      if (anyNA(y)) {
+        y <- na.omit(y)
+      }
+      if (object.size(y) > 500 * 1000)
+        stop("y is too large, please subsection the vector")
+      y <- unique(y)
     }
-    if( object.size(y) > 500 * 1000) stop("y is too large, please subsection the vector")
-    y <- unique(y)
-   }
   }
   x <- tolower(x)
   y <- tolower(y)
-  if(length(setdiff(x,y))==0) {
-    dist <- as.matrix(stringdist::stringdistmatrix(x, method = 'osa', weight = c(1,1,1,1), useNames = "string" ))
+  if (length(setdiff(x, y)) == 0) {
+    dist <-
+      as.matrix(stringdist::stringdistmatrix(
+        x,
+        method = 'osa',
+        weight = c(1, 1, 1, 1),
+        useNames = "string"
+      ))
   } else {
-  xm <- x[!x %in% y]
-  if (any(nchar(xm) < 5)){
-    xms <- xm[nchar(xm) < 5]
-    xml <- xm[nchar(xm) >= 5]
-    if(length(list(...))) {
-      dists <- as.matrix(stringdist::stringdistmatrix(unique(xms), y, useNames = "string", ... ))
-      distl <- as.matrix(stringdist::stringdistmatrix(unique(xml), y, useNames = "string", ... ))
+    xm <- x[!x %in% y]
+    if (any(nchar(xm) < 5)) {
+      xms <- xm[nchar(xm) < 5]
+      xml <- xm[nchar(xm) >= 5]
+      if (length(list(...))) {
+        dists <-
+          as.matrix(stringdist::stringdistmatrix(unique(xms), y, useNames = "string", ...))
+        distl <-
+          as.matrix(stringdist::stringdistmatrix(unique(xml), y, useNames = "string", ...))
+      } else {
+        dists <-
+          as.matrix(
+            stringdist::stringdistmatrix(
+              unique(xms),
+              y,
+              method = 'osa',
+              weight = c(1, .10, 1, 1),
+              useNames = "string"
+            )
+          )
+        distl <-
+          as.matrix(
+            stringdist::stringdistmatrix(
+              unique(xml),
+              y,
+              method = 'osa',
+              weight = c(1, 1, 1, 1),
+              useNames = "string"
+            )
+          )
+      }
+      dist <- rbind(dists, distl)
     } else {
-      dists <- as.matrix(stringdist::stringdistmatrix(unique(xms), y, method = 'osa', weight = c(1,.10,1,1), useNames = "string" ))
-      distl <- as.matrix(stringdist::stringdistmatrix(unique(xml), y, method = 'osa', weight = c(1,1,1,1), useNames = "string" ))
+      dist <-
+        as.matrix(
+          stringdist::stringdistmatrix(
+            unique(xm),
+            y,
+            method = 'osa',
+            weight = c(1 , 1, 1, 1),
+            useNames = "string"
+          )
+        )
     }
-    dist <- rbind(dists,distl)
-  } else {
-    dist <- as.matrix(stringdist::stringdistmatrix(unique(xm), y, method = 'osa', weight = c(1 ,1, 1,1), useNames = "string" ))
-  }
   }
   dist <- reshape2::melt(dist)
-  names(dist)<- c("original_value","matched_value","distance")
-  dist$original_value<- as.character(dist$original_value)
+  names(dist) <- c("original_value", "matched_value", "distance")
+  dist$original_value <- as.character(dist$original_value)
   dist$matched_value <- as.character(dist$matched_value)
   dist <- dist[dist$distance <= threshold & dist$distance != 0,]
   row.names(dist) <- NULL
-return(dist)
+  return(dist)
 }
 #' @title Convert data.frames to excel workbook
 #'
@@ -125,16 +172,26 @@ return(dist)
 #' @family Utilities
 #' @export
 save.xlsx <- function (file = getwd(), ...) {
-  if(missing(file)) file <- getwd()
+  if (missing(file))
+    file <- getwd()
   objects <- list(...)
   fargs <- as.list(match.call(expand.dots = TRUE))
   objnames <- as.character(fargs)[-c(1, 2)]
   nobjects <- length(objects)
   for (i in 1:nobjects) {
     if (i == 1)
-      xlsx::write.xlsx(as.data.frame(objects[[i]]), file, sheetName = objnames[i],row.names = FALSE)
-    else xlsx::write.xlsx(as.data.frame(objects[[i]]), file, sheetName = objnames[i], row.names = FALSE,
-                          append = TRUE)
+      xlsx::write.xlsx(as.data.frame(objects[[i]]),
+                       file,
+                       sheetName = objnames[i],
+                       row.names = FALSE)
+    else
+      xlsx::write.xlsx(
+        as.data.frame(objects[[i]]),
+        file,
+        sheetName = objnames[i],
+        row.names = FALSE,
+        append = TRUE
+      )
   }
   print(paste("Workbook", file, "has", nobjects, "worksheets."))
 }
@@ -156,17 +213,17 @@ save.xlsx <- function (file = getwd(), ...) {
 #' @family Utilities
 #' @export
 round2 <- function(x, digits = 5L) {
-  if(missing(x)||!is.numeric(x)) {
+  if (missing(x) || !is.numeric(x)) {
     stop("Please define x as a number")
   }
-  if(!is.integer(digits)) {
+  if (!is.integer(digits)) {
     stop("Please define digits as an integer")
   }
   posneg <- sign(x)
-  z <- abs(x)* 10^digits
+  z <- abs(x) * 10 ^ digits
   z <- z + 0.5
   z <- trunc(z)
-  z <- z/ 10^digits
+  z <- z / 10 ^ digits
   z * posneg
 }
 #' @title Trim trailing and leading whitespace from a character vector
@@ -186,9 +243,11 @@ round2 <- function(x, digits = 5L) {
 #' @family Utilities
 #' @export
 trim <- function(x) {
-  if(missing(x)||!is.character(x)) stop("Please define x as a character vector")
+  if (missing(x) ||
+      !is.character(x))
+    stop("Please define x as a character vector")
   x <- gsub("^\\s+|\\s+$", "", x)
- return(x)
+  return(x)
 }
 
 #' @title Working item that will allow the pasting of strings
@@ -197,8 +256,10 @@ paste_assign <- function() {
   cat('Paste search string and hit enter twice')
   x <- scan(what = "")
   xa <- gsub('\\\\', '/', x)
-  writeClipboard(paste(xa, collapse=" "))
-  cat('Here\'s your de-windowsified path. (It\'s also on the clipboard.)\n', xa, '\n')
+  writeClipboard(paste(xa, collapse = " "))
+  cat('Here\'s your de-windowsified path. (It\'s also on the clipboard.)\n',
+      xa,
+      '\n')
 }
 
 #' @title Download github files from GitItSheaGitIt
@@ -206,12 +267,25 @@ paste_assign <- function() {
 #' @author Shea Fyffe, \email{shea.fyffe@@gmail.com}
 #' @family Utilities
 #' @export
-download_code <- function (git_file) {
+download_code <- function(git_file) {
   tmp <- tempfile()
-  .git_file <- sprintf("https://raw.githubusercontent.com/Shea-Fyffe/GitItSheaGitIt/master/%s", git_file)
-  .git_file  <- tryCatch(RCurl::getURL(.git_file, ssl.verifypeer = FALSE), error=function(err) {NA})
-  if(is.na(.git_file)) {
-    stop(sprintf("Error pulling file verify that %s is a valid file name", git_file))
+  .git_file <-
+    sprintf(
+      "https://raw.githubusercontent.com/Shea-Fyffe/GitItSheaGitIt/master/%s",
+      git_file
+    )
+  .git_file  <-
+    tryCatch(
+      RCurl::getURL(.git_file, ssl.verifypeer = FALSE),
+      error = function(err) {
+        NA
+      }
+    )
+  if (is.na(.git_file)) {
+    stop(sprintf(
+      "Error pulling file verify that %s is a valid file name",
+      git_file
+    ))
   }
   con <- file(tmp, "w")
   writeLines(.git_file, con = con)
