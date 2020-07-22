7e2de36151010364b3fe52b9fe5dc98147bccae4
Shea-Fyffe <shea.fyffe@gmail.com>
2020-07-22 10:03:50 -0400

    shea.fyffe: reformatted code; need to add robust documentation; added word embeddings

--- R/machine_learning_functions.R
+++ R/machine_learning_functions.R
@@ -1,3 +1,4 @@
+#' todo(shea.fyffe) add proper documenation
 #' @title
 #' @author 
 #' @param x 
@@ -28,15 +29,64 @@ preprocess_text <- function(x, expand_contractions = TRUE, split = NULL, ...) {
 #' @export
 #'
 #' @examples
-create_lexicon <- function(x, ...) {
+create_lexicon <- function(x, doc_prop_max = 1, word_min = 5, word_max = Inf, ...) {
   if(!is.character(x)) {
     stop("x is not a character vector")
   }
-  .it <- text2vec::itoken(x, pro)
+  .it <- text2vec::itoken(x,  progressbar = FALSE)
   .vocab <- text2vec::create_vocabulary(.it)
-  .vocab <- text2vec::prune_vocabulary(.vocab, ...)
+  .vocab <- text2vec::prune_vocabulary(.vocab, doc_proportion_max = doc_prop_max,
+                                       term_count_min = word_min,
+                                       term_count_max = word_max, ...)
   .vec <- text2vec::vocab_vectorizer(.vocab)
-  return(.vec)
+  return(list(tkn = .it, vec = .vec, vocab = .vocab))
+}
+#' @title
+#'
+#' @param text 
+#' @param skip_win 
+#' @param co_occur_max 
+#' @param word_vec_len 
+#' @param ... 
+#'
+#' @return
+#' @export
+#'
+#' @examples
+create_gloVe_embeddings <- function(text, skip_win = 5L, co_occur_max = 100L, word_vec_len = 50L, ...){
+  .x <- create_lexicon(text, ...)
+  .dtm <-  text2vec::create_tcm(.x[[1]], .x[[2]], skip_win)
+  .gloVe <- text2vec::GlobalVectors$new(word_vectors_size = word_vec_len, vocabulary = .x[[3]], x_max = co_occur_max)
+  .wd_vec <- text2vec::fit_transform(x = .dtm, model = .gloVe, n_iter = 10L, convergence_tol = 0.01)
+  .wd_vec_context <- .gloVe$components
+  .word_vectors <- .wd_vec + t(.wd_vec_context)
+  return(as.data.frame(.word_vectors))
+}
+#' @title
+#'
+#' @param text 
+#' @param word_vec 
+#' @param has_words 
+#' @param mean_vec 
+#'
+#' @return
+#' @export
+#'
+#' @examples
+create_doc_vectors <- function(text, word_vec, has_words = FALSE, mean_vec = TRUE) {
+  if(!inherits(word_vec, "data.frame")) {
+    word_vec <- as.data.frame(word_vec)
+  }
+  if(!has_words) {
+    word_vec <- data.frame(row.names(word_vec), word_vec)
+  }
+  if(!all(sapply(word_vec[-1], is.numeric))) {
+    word_vec[-1] <- sapply(word_vec[-1], as.numeric)
+  }
+  .res <- sapply(text, function(.x) {
+    .x <- softmaxreg::wordEmbed(.x, word_vec, meanVec = mean_vec)
+  })
+  return(t(.res))
 }
 
 plot_rstne <- function(mat, group, labs, ...) {
